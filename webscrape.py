e54d305a1d66b0125a95fcd7a4eafb11408932b8
John <johnitolee@gmail.com>
2018-03-28 22:42:39 -0700

    implement simple request example

--- webscrape.py
+++ webscrape.py
@@ -1,4 +1,9 @@
-import bs4
-from urllib2 import urlopen
+from urllib2 import urlopen as req
+from bs4 import BeautifulSoup as soup 
 
+my_url = 'https://sfbay.craigslist.org/search/sfc/roo?max_price=1100&availabilityMode=0&private_room=1'
 
+client_response = req(my_url)
+page_html = client_response.read()
+
+print(client_response)
